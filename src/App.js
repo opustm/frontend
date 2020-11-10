1ca30141064a5663ff47731da0dc2704a333b109
Swopnil N. Shrestha <7950326+swopnilnep@users.noreply.github.com>
2020-11-10 12:47:29 -0600

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Login from './components/login/login.component';
+import Groups from './components/groups/group.component';
+import Home from './components/home/home.component';
+import About from './components/about/about.component';
 
 function App(props) {
     return (
-        <Router>
-            <Login/>
-        </Router>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path = "/" component = {Home}>
+                    <h3>Home</h3>
+                </Route>
+                <Route path = "login" component = {Login}/>
+                <Route path = "about" component = {About}/>
+                <Route path = "groups" component = {Groups}/>
+            </Switch>
+        </BrowserRouter>
     )
 }
 
---
+++ src/components/groups/group.component.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Groups(props) {
+    return
+        <h1>Groups</h1>
+}
+
+export default Groups;
