01087f62d7d994683611b9bf6d86750267ba25d2
Swopnil N. Shrestha <7950326+swopnilnep@users.noreply.github.com>
2020-11-12 14:49:33 -0600

    Add new team component

---
+++ src/components/team/team.component.js
@@ -0,0 +1,57 @@
+import { Container, Navbar, Nav, Row, Jumbotron, Button, Col, Form, Card, ListGroup, ListGroupItem} from 'react-bootstrap';
+import * as Icon from 'react-feather';
+import NavigationBar from '../overlay/navbar.component';
+
+// Team
+// Group
+
+export default function Groups(props) {
+    return (
+      <Container fluid>
+        <Row>
+          <Col>
+            <NavigationBar/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Jumbotron>
+              <h1>Create or Join a Team</h1>
+              <p>
+                Instructions erspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, tium doloremque laudantium, tot
+              </p>
+                <Form.Row>
+                <Col>
+                  <p>
+                    <Button variant="success"><Icon.Plus/> New Team</Button>
+                  </p>
+                </Col>
+                <Col>
+                  <Form.Control type="text" placeholder="Enter a code or link"/>
+                </Col>
+                <Col>
+                  <Button variant="primary" type="submit">Join</Button>
+                </Col>
+                </Form.Row>
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <Card>
+          <Card.Header><Card.Title>Your Teams</Card.Title>
+          <small>You are a member in the following teams</small>
+          </Card.Header>
+          <Card.Body>
+            <ListGroup className="list-group-flush">
+            <ListGroupItem>Cras justo odio</ListGroupItem>
+            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+          </Col>
+        </Row>
+      </Container>
+    )
+}
