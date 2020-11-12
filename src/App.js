16ea560d0342d8107ac3c618a862fa2af5d1c191
Swopnil N. Shrestha <7950326+swopnilnep@users.noreply.github.com>
2020-11-12 14:57:28 -0600

    Change stylesheet dependencies

--- src/App.js
+++ src/App.js
@@ -2,50 +2,50 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 import Login from './components/login/login.component';
-import Groups from './components/groups/group.component';
+import Groups from './components/team/team.component';
 import Home from './components/home/home.component';
 import About from './components/about/about.component';
 
-
 export default function App() {
     return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/groups">Groups</Link>
-              </li>
-            </ul>
-          </nav>
-  
-          {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/groups">
-              <Groups />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <div>
+        <Router>
+            {/* <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/groups">Groups</Link>
+                </li>
+              </ul>
+            </nav> */}
+    
+            {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/team">
+                <Groups />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+
+        </Router>
+      </div>
     );
   }
   
