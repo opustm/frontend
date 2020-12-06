3daf926c87ed2257a5e8e0fdacd0a1f3e4cd6933
Swopnil Shrestha <commits@swopnil.com>
2020-12-06 00:49:11 -0600

    Add App stylesheets

---
+++ src/components/apps/Teams/Teams.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Teams() {
+    return (
+        <div className="page">
+            <h1>Teams</h1>
+        </div>
+
+    )
+}
+
+export default Teams;
---
+++ src/components/apps/chat/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Chat() {
+    return (
+        <div className="page">
+            <h1>Chat</h1>
+        </div>
+
+    )
+}
+
+export default Chat;
