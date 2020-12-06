1b1999e3ec6469c5539af09176899e74d64057b3
Swopnil Shrestha <commits@swopnil.com>
2020-12-05 23:42:45 -0600

    Add origin

---
+++ src/components/apps/Announcements/Announcements.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Announcements() {
+    return (
+        <div className="page">
+            <h1>Announcements</h1>
+        </div>
+
+    )
+}
+
+export default Announcements;
