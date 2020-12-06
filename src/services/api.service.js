d32b6e837beff9d115fad525ea8c255cd687e45d
Swopnil Shrestha <commits@swopnil.com>
2020-12-06 12:19:24 -0600

    Modify api url

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -15,7 +15,7 @@ function locationType() {
 
 function APIHost() {
     return (locationType() < 2) ? 
-    'http://localhost:8000/': 'http://localhost:8000/';
+    'http://localhost:8000/': 'http://opustm-api-staging.herokuapp.com/';
 }
 
 export default APIHost;
