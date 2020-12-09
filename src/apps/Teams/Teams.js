a7f333e69cda975a9d86c6b43345485a8c1085f8
Swopnil Shrestha <7950326+swopnilnep@users.noreply.github.com>
2020-12-09 15:36:03 -0600

    Display data in Teams

--- src/apps/Teams/Teams.js
+++ src/apps/Teams/Teams.js
@@ -1,37 +1,69 @@
-import {Container, Jumbotron, Button, Form, Card, ListGroup, ListGroupItem} from 'react-bootstrap';
-import * as Icon from 'react-feather';
-
-// Team
-// Group
+import React, { useState, useEffect } from 'react';
+import {Container, Jumbotron, Form, Button, Card, Table} from 'react-bootstrap';
+import { Axios as api, API_ENDPOINTS as urls } from '../../services/api.service';
+import * as Icon from 'react-icons/fi';
+import './teams.css';
 
 export default function Teams(props) {
-    return (
-      <Container fluid>
-            <Jumbotron>
-              <h1>Create or Join a Team</h1>
-              <p>
-                Instructions erspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, tium doloremque laudantium, tot
-              </p>
-                <Form.Row>
-                  <p>
-                    <Button variant="success"><Icon.Plus/> New Team</Button>
-                  </p>
-                  <Form.Control type="text" placeholder="Enter a code or link"/>
-                  <Button variant="primary" type="submit">Join</Button>
-                </Form.Row>
-            </Jumbotron>
-          <Card>
-          <Card.Header><Card.Title>Your Teams</Card.Title>
-          <small>You are a member in the following teams</small>
-          </Card.Header>
-          <Card.Body>
-            <ListGroup className="list-group-flush">
-            <ListGroupItem>Cras justo odio</ListGroupItem>
-            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-            <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-          </Card.Body>
-        </Card>
-      </Container>
-    )
+  const [teams,setTeams] = useState([0]);
+  useEffect(() => {
+    async function fetchTeams() {
+      const request = await api.get(urls.teams.fetchAll);
+      setTeams(request.data);
+      console.log(request.data);
+      return request;
+    }
+    try{
+      fetchTeams();
+    }
+    catch (err) {
+      console.log(`Error while fetching Teams ${err}`);
+      setTeams([]);
+    }
+  }, []); // Dependencies need to be included in useEffect
+
+  return (
+    <Container fluid>
+          <Jumbotron>
+            <h1>Create or Join a Team</h1>
+            <p>
+              Instructions erspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, tium doloremque laudantium, tot
+            </p>
+              <Form.Row>
+                <p>
+                  <Button variant="success"><Icon.FiPlus/> New Team</Button>
+                </p>
+                <Form.Control type="text" placeholder="Enter a code or link"/>
+                <Button variant="primary" type="submit">Join</Button>
+              </Form.Row>
+          </Jumbotron>
+        <Card>
+        <Card.Header><Card.Title>Your Teams</Card.Title>
+        <small>You are a member in the following teams</small>
+        </Card.Header>
+        <Card.Body>
+          <Table striped hover>
+            <tbody>
+              {teams.map((item,index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <img src={item.picture}
+                        className="avatar"
+                        alt="Team profile avatar"/>
+                    </td>
+                    <td>
+                      {item.name}
+                    </td>
+                    <td>
+                      {item.cliqueType}
+                    </td>
+                  </tr>
+                )})}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </Container>
+  )
 }
