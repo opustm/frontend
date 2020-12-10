f7390a70937d814b92b08e13ae84e0027721d807
Swopnil Shrestha <7950326+swopnilnep@users.noreply.github.com>
2020-12-09 21:58:02 -0600

    Add not found page

---
+++ src/Pages/NotFound.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Error = (props) => 
+    <div>
+        <Container>
+        <img 
+            src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif"
+            alt="Not found animation"/>
+        <h2>Looks like you're lost.</h2>
+        <p>The page you are looking for is not available.</p>
+        <Link to="/">
+            <Button>
+                Go Home
+            </Button>
+        </Link>
+        </Container>
+    </div>
+
+export default Error;
