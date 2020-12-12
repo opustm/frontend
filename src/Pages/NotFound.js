ff2eea8d3dbc72a84e71ba2232841839bf663e71
David Will <willda07@luther.edu>
2020-12-12 15:03:39 -0600

    Start/finish contacts page

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import React from 'react';
+import { Button, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import '../stylesheets/Error.css';
 
