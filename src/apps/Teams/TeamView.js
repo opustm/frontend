f484b57e82fa6a010287a680828da47fc8eab449
Swopnil Shrestha <7950326+swopnilnep@users.noreply.github.com>
2020-12-13 23:51:41 -0600

    Update Teams UI

--- src/apps/Teams/TeamView.js
+++ src/apps/Teams/TeamView.js
@@ -1,34 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import { Axios as api, API_ENDPOINTS as urls } from '../../services/api.service';
+import { Container, Col, Jumbotron } from 'react-bootstrap';
 import { Link, Redirect, useParams } from 'react-router-dom';
 import './teams.css';
 
 const TeamView = (props) => {
-    const [isLoading,setIsLoading] = useState(true);
     const [details,setDetails] = useState();
     const [members,setMembers] = useState();
-    let teamUsername = useParams();
-
+    const [groups,setGroups] = useState();
+    let teamUsername = useParams().teamUsername;
+    
     useEffect(() => {
         async function fetchDetails() {
             const request = await api.get(
                 urls.teams.fetchDetails(teamUsername)
-                )
+                );
             setDetails(request.data);
-            console.log(request.data);
-            return request;
+            console.log("Details", request.data);
+        }
+        async function fetchMembers() {
+            const request = await api.get(
+                urls.teams.fetchMembers(teamUsername)
+            );
+            setMembers(request.data);
+            console.log("Members", request.members);
+        }
+        async function fetchGroups() {
+            const request = await api.get(
+                urls.teams.fetchRelatedTeams(teamUsername)
+            );
+            setGroups(request.data);
+            console.log("Groups", request.data);
         }
         try {
             fetchDetails();
+            fetchMembers();
+            fetchGroups();
         }
         catch (err) {
             <Redirect to="/404"/>
         }
     }, [teamUsername]);
     
-    return (
+    let detailsView = 
+        <Container>Details</Container>
+
+    let membersView = 
+        <Container>Members</Container>
+
+    let basePage =
         <div className="page">
-            <h1>Template</h1>
+            <Container fluid>
+                <Col sm={12} md={{span: 10, offset: 1}}>
+                    <Jumbotron>
+                        <h1>{details.name? details.name: null}</h1>
+                        <p>This is a team for so and so and so purposes. </p>
+                    </Jumbotron>
+                </Col>
+
+            </Container>
+        </div>
+
+    return (
+        <div>
+            {basePage}
         </div>
     )
 }
--- src/apps/Teams/Teams.js
+++ src/apps/Teams/Teams.js
@@ -11,7 +11,6 @@ const Teams = () => {
     async function fetchTeams() {
       const request = await api.get(urls.teams.fetchAll);
       setTeams(request.data);
-      console.log(request.data);
       return request;
     }
     try{
@@ -24,31 +23,52 @@ const Teams = () => {
 
   let teamsView =      
     <Container className="teams-container">        
-      {teams.map((item,index) => {
-        return (
-          <div key={index} className="team-element">
-          <Row>
-            <Link to="#">
-            <img 
-              className="team-photo avatar"
-              alt="Team logo"
-              src="https://via.placeholder.com/40/555555?text=T"/>
-              <h4>{item.name}</h4>
-            </Link>
-            <Dropdown>
-              <Dropdown.Toggle variant="outline-primary">
-                <Icon.FiSettings/>
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item href="#">Edit</Dropdown.Item>
-                <Dropdown.Item href="#">Leave</Dropdown.Item>
-                <Dropdown.Item href="#">Delete</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Row>
-          </div>
-        )
-      })}
+      <ul>
+        {teams.map((item,index) => {
+          return (
+            <div key={index} className="team-element">
+              <li className="col-12 d-flex width-full border-bottom color-border-secondary">
+                <Col md={1} lg={1} className="d-flex justify-content-center">
+                  <Link to={`/teams/${item.name}`}>
+                    <img 
+                      className="team-photo avatar"
+                      alt="Team logo"
+                      src="https://via.placeholder.com/40/555555?text=T"/>
+                  </Link>
+                </Col>
+                <Col md={10} lg={9}>
+                  <Row>
+                    <div className="d-inline-block mb-1">
+                      <h4 className="wb-break-all">
+                        <Link to={`teams/${item.name}`}>
+                          {item.name}
+                        </Link>
+                      </h4>
+                    </div>
+                  </Row>
+                  <Row className="small">
+                    This is a description for the team {item.name}
+                  </Row>
+                </Col>
+                <Col md={1} lg={1} className="text-right">
+                  <div className="d-inline-block mb-1">
+                    <Dropdown>
+                    <Dropdown.Toggle variant="small primary">
+                      <Icon.FiSettings/>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item href="#">Edit</Dropdown.Item>
+                      <Dropdown.Item href="#">Leave</Dropdown.Item>
+                      <Dropdown.Item href="#">Delete</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                  </div>
+                </Col>
+              </li> 
+            </div>
+          )
+        })}
+      </ul>
     </Container>
 
   return (
@@ -59,14 +79,14 @@ const Teams = () => {
           <p>
             View or modify your current teams. Create or join a new team. <Link to="/docs">Need more info? Read the docs.</Link>
           </p>
-          <p>
+          <div>
             <ButtonGroup className="mr-2">
               <Button variant="primary"><Icon.FiUsers/> Create Team</Button>
             </ButtonGroup>
             <ButtonGroup className='mr-2'>
               <Button variant="success"><Icon.FiPlus/> Join Team</Button>
             </ButtonGroup>
-          </p>
+          </div>
         </Jumbotron>
         {teams? teamsView : <p>It's empty here. You are not a member of any teams yet. Create or Join a team above.</p>}
       </Col>
