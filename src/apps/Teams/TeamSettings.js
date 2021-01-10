d60e01e38059e8662af16f84f34aa8795fbc6b33
Swopnil Shrestha <7950326+swopnilnep@users.noreply.github.com>
2021-01-10 14:32:48 -0600

    Remove unused dependencies from TeamSettings

--- src/apps/Teams/TeamSettings.js
+++ src/apps/Teams/TeamSettings.js
@@ -1,14 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Axios as api, API_ENDPOINTS as urls } from '../../services/api.service';
-import { Container, Card, ListGroup, Dropdown, Button, Row, Col, Jumbotron, Image } from 'react-bootstrap';
-import { Link, Redirect, useParams } from 'react-router-dom';
-import * as Icon from 'react-icons/fi';
-import './teams.css';
 
 const TeamSettings = (props) => {
-    let teamUsername = useParams().teamUsername;
-    const [members,setMembers] = useState();
-    const [groups,setGroups] = useState();
 
     useEffect(() => {
 
