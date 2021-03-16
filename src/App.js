8c1ca65c5dbaf0d1f750ad76e526bb2672107eeb
David Will <willda07@luther.edu>
2021-03-15 21:23:54 -0500

    Edit npm test script, delete unused files

--- src/App.js
+++ src/App.js
@@ -20,10 +20,7 @@ import Profile from './Pages/Profile';
 // Apps
 import Teams from './apps/Teams/Teams';
 import TeamView from './apps/Teams/TeamView';
-import TeamSettings from './apps/Teams/TeamSettings';
 import Calendar from './apps/Calendar/Calendar';
-import Scheduler from './apps/Scheduler/Scheduler';
-import Chat from './apps/Chat/Chat';
 import Contacts from './apps/Contacts/Contacts';
 import Announcements from './apps/Announcements/Announcements';
 
@@ -81,13 +78,10 @@ export default function App() {
             <div className={sidebarToggled? "page sidebar-toggled":"page"}>
               <Route path="/" exact component={() => {return <Home userInfo={userData}/>}}/>
               <Route path="/calendar/:teamUsername?" exact component={(props) => {return <Calendar {...props} userInfo={userData}/>}}/>
-              <Route path="/scheduler" exact component={Scheduler}/>
               <Route path="/teams" exact component={(props) => {return <Teams userInfo={userData} updateTeams={updateTeams}/>}}/>
-              <Route path="/chat" exact component={Chat}/>
               <Route path="/contacts" exact component={() => {return <Contacts userInfo={userData}/>}}/>
               <Route path="/announcements/:teamUsername?" exact component={(props) => {return <Announcements {...props} userInfo={userData}/>}}/>
               <Route path="/user/:username" component={(props) => {return <Profile {...props} userInfo={userData} />}}/>
-              <Route path="/teams/:teamUsername/settings" exact component={TeamSettings}/>
               <Route path="/teams/:teamUsername" exact component={(props) => {return <TeamView userInfo={userData} updateTeams={teamViewUpdate}/>}}/>
             </div>
             : <Redirect to='/about' />
