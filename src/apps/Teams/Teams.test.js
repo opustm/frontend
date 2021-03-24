929a34a71eeea014006dfed2dfc725de39555f33
David Will <willda07@luther.edu>
2021-03-24 15:44:34 -0500

    Initial push to create specific cases for Teams component

--- src/apps/Teams/Teams.test.js
+++ src/apps/Teams/Teams.test.js
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { Axios } from '../../services/api.service';
-import { render, screen } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import mockAPI from '../../services/test.service';
 import Teams from './Teams';
@@ -14,25 +14,54 @@ jest.mock('../../services/api.service');
 //    Intercept corresponding POST, check that new team is displayed
 // Team deletion
 //    Intercept corresponding DELETE, check that the team was removed
+// Leave team
+// Verify modal errors for both create and join
 
-beforeEach(() => {
-  // Axios.get.mockResolvedValueOnce(mockAPI.userTeams);
-  // render(
-  //   <Router>
-  //     <Teams userInfo={mockAPI.userInfo} />
-  //   </Router>
-  // )
+describe('Tests with successful API data', () => {
+  beforeEach(() => {
+    Axios.get.mockResolvedValueOnce(mockAPI.userTeams).mockResolvedValueOnce(mockAPI.userTeams);
+    render(
+      <Router>
+        <Teams userInfo={mockAPI.userInfo} />
+      </Router>
+    )
+  })
+  
+  test('Teams component correctly renders data', async () => {
+    let foundText = screen.getByText('CS 150');
+    expect(foundText).toBeInTheDocument();
+  });
+
+  test('Team creation is successful', () => {
+    let createButton = screen.getByText('Create Team');
+    // Click the create button
+    fireEvent(
+      createButton,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: false
+      })
+    );
+    expect(screen.getByText('Team Name')).toBeInTheDocument();
+  });
+
+//   test('Team deletion is successful', () => {
+
+//   });
+
+//   test('Team join is successful', () => {
+
+//   });
+  
+//   test('Leaving a team is successful', () => {
+
+//   });
+
+//   test('Verify create modal errors', () => {
+
+//   });
+
+//   test('Verify join modal errors', () => {
+
+//   });
 })
-
-test('Teams component correctly renders data', () => {
-  // expect(screen.getByText('CS 150')).toBeInTheDocument();
-  expect(true).toBeTruthy();
-});
-
-// test('Team creation is successful', () => {
-
-// })
-
-// test('Team deletion is successful', () => {
-
-// });
